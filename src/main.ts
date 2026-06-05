6d9e68368d36322e3d6fc07cd4a1cf05ddecf1c0
Dinesh Saravanan <dineshsaravanan93@gmail.com>
2026-06-05 18:49:59 +0530

    fix: run production build on Render start:dev command and bind to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT ?? 4000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger documentation is available at: http://localhost:${port}/api/docs`);
 }
